1403c3414d620236f213f46629a50e3bb31fcec7
ARJUN <aa6709@srmist.edu.in>
2026-02-18 15:45:31 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,21 +5,10 @@ public class PalindromeCheckerApp {
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully");
     }
-    boolean checkpalin(){
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter a string: ");
-        String sample=input.nextLine();
-        String rev="";
 
-        for (int i=sample.length()-1;i>=0;i--){
-            rev+=sample.charAt(i);
-        }
-        return sample.equals(rev);
-    }
     public static void main(String []args){
         PalindromeCheckerApp ob=new PalindromeCheckerApp();
         ob.show();
-        System.out.println("Is it a Palindrome: " +ob.checkpalin());
     }
 }
 
